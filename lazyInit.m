ffd451e4301ef337cbab7ce0d8b26037687251ff
MBALearnsToCode <MBALearnsToCode@gmail.com>
2014-06-05 18:50:01 +0800

    commit #002

--- lazyInit.m
+++ lazyInit.m
@@ -2,7 +2,7 @@
 % ----------
 PS1('cmd> '); clear; close all; clc;
 
-cd 'C:\Users\PC\Desktop';
+cd 'D:\Cloud\Box Sync\GitHub\MBALearnsToCode\MatlabOctave';
 addpath 'D:\Programs\Octave-3.6.4';
 addpath(genpath('D:\Cloud\Box Sync\GitHub\MBALearnsToCode\MatlabOctave'));
 
