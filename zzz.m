4bdf1b84de0ef89c5f0077bcf75cdce0621b139b
MBALearnsToCode <MBALearnsToCode@UChicago.edu>
2014-09-05 22:07:17 -0500

    fresh commit

--- zzz.m
+++ zzz.m
@@ -3,6 +3,7 @@
 PS1('CMD>>>   '); clear; close all; clc;
 
 cd 'D:\Cloud\Dropbox\MBALearnsToCode\MatlabOctave';
-addpath(genpath('D:\Cloud\Dropbox\MBALearnsToCode\MatlabOctave'));
+addpath(genpath...
+   ('D:\Cloud\Dropbox\MBALearnsToCode\MatlabOctave'));
 
 more off;
