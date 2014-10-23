e0c938b14f0c6a021a389253002f707b06a05b52
MBALearnsToCode <MBALearnsToCode@UChicago.edu>
2014-10-22 21:23:44 -0500

    2014-10-22 21:24 ffNN package changes for numerical stability

---
+++ ffNN/@class_ffNN/avgWeights_byConnectivity.m
@@ -0,0 +1,13 @@
+function m = avgWeights_byConnectivity(ffNN, ...
+   connectivity_byLayer = [1.0])
+
+   m = ffNN;   
+   for (l = 1 : m.numTransforms)   
+      if (l > 1) && isempty(connectivity_byLayer(l))
+         connectivity_byLayer(l) = ...
+            connectivity_byLayer(l - 1);
+      endif   
+      m.weights{l} *= connectivity_byLayer(l);   
+   endfor
+      
+endfunction
