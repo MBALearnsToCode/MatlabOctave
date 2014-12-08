53b220877654a3ee0d890871729fa6e88a6723e7
MBA Learns to Code <MBALearnsToCode@UChicago.edu>
2014-12-08 13:03:40 -0600

    2014-12-08 packages updated

---
+++ instPkgs.m
@@ -0,0 +1,16 @@
+function instPkgs
+
+   pkg install -forge general;
+   pkg install -forge io;
+   pkg install -forge nan;
+   pkg install -forge signal;
+   pkg install -forge dataframe;
+   pkg install -forge statistics;
+   pkg install -forge econometrics;
+   pkg install -forge linear-algebra;
+   pkg install -forge struct;
+   pkg install -forge optim;
+   pkg install -forge ga;
+   pkg install -forge nnet;
+
+endfunction
